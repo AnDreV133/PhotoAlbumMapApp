938fe4a1a915e680776ee3005d1b9db2f6535464
AnDreV133 <dmitrijkubik@gmail.com>
2025-10-10 23:07:21 +0300

    fix: изменена модель

--- app/src/main/java/com/andrev133/photoalbummapapp/domain/model/PhotoCollectionMarkerModel.kt
+++ app/src/main/java/com/andrev133/photoalbummapapp/domain/model/PhotoCollectionMarkerModel.kt
@@ -1,21 +1,26 @@
 package com.andrev133.photoalbummapapp.domain.model
 
+import androidx.compose.ui.graphics.Color
 import com.andrev133.photoalbummapapp.data.relation.CollectionWithMarkerRelation
 import com.andrev133.photoalbummapapp.data.relation.FullCollectionRelation
+import com.andrev133.photoalbummapapp.domain.util.getColorWithFullAlpha
 import com.andrev133.photoalbummapapp.domain.util.getPointFromJsonString
+import kotlinx.datetime.Clock
 import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.todayIn
 import ru.sulgik.mapkit.geometry.Point
 
 class PhotoCollectionMarkerModel(
-    val colorCode: Int,
+    val color: Color,
     val title: String,
-    val time: LocalDate,
-    val point: Point,
-    val photos: List<PhotoModel>
+    val time: LocalDate = Clock.System.todayIn(TimeZone.currentSystemDefault()),
+    val point: Point = Point(0.0, 0.0),
+    val photos: List<PhotoModel> = emptyList()
 )
 
 fun FullCollectionRelation.toModel() = PhotoCollectionMarkerModel(
-    colorCode = marker.colorCode,
+    color = getColorWithFullAlpha(marker.colorCode),
     title = marker.title,
     time = LocalDate.parse(collection.time),
     point = getPointFromJsonString(collection.mapPoint),
@@ -23,7 +28,7 @@ fun FullCollectionRelation.toModel() = PhotoCollectionMarkerModel(
 )
 
 fun CollectionWithMarkerRelation.toModel() = PhotoCollectionMarkerModel(
-    colorCode = marker.colorCode,
+    color = getColorWithFullAlpha(marker.colorCode),
     title = marker.title,
     time = LocalDate.parse(collection.time),
     point = getPointFromJsonString(collection.mapPoint),
