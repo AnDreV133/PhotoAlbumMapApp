7b5f828aa770e2f389a362b552aa80feae6bb8ea
AnDreV133 <dmitrijkubik@gmail.com>
2025-10-24 11:11:20 +0300

    feat: обновлён MainActivity

--- app/src/main/java/com/andrev133/photoalbummapapp/app/MainActivity.kt
+++ app/src/main/java/com/andrev133/photoalbummapapp/app/MainActivity.kt
@@ -16,7 +16,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,14 +24,18 @@ import com.andrev133.photoalbummapapp.BuildConfig
 import com.andrev133.photoalbummapapp.app.compose.FooterBar
 import com.andrev133.photoalbummapapp.app.compose.MapView
 import com.andrev133.photoalbummapapp.app.compose.MarkerListDialog
+import com.andrev133.photoalbummapapp.app.compose.MarkerListDialogWithAdd
 import com.andrev133.photoalbummapapp.app.ui.theme.AutoMediaAppTheme
 import com.andrev133.photoalbummapapp.data.AppDatabase
-import com.andrev133.photoalbummapapp.domain.model.PhotoCollectionMarkerModel
+import com.andrev133.photoalbummapapp.domain.usecase.AddMarkerOnMapUseCase
+import com.andrev133.photoalbummapapp.domain.usecase.DeleteMarkerOnMapUseCase
+import com.andrev133.photoalbummapapp.domain.usecase.GetAllCollectionsWithMarkerUseCase
 import com.andrev133.photoalbummapapp.domain.usecase.GetAllMarkersUseCase
 import com.andrev133.photoalbummapapp.domain.usecase.InsertMarkerUseCase
 import ru.sulgik.mapkit.MapKit
 import ru.sulgik.mapkit.compose.bindToLifecycleOwner
 import ru.sulgik.mapkit.compose.rememberAndInitializeMapKit
+import ru.sulgik.mapkit.compose.rememberCameraPositionState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,16 +61,24 @@ fun MainScreen() {
 
     val db = AppDatabase.getInstance(LocalContext.current)
     val getAllMarkersUseCase = GetAllMarkersUseCase(db.markerDao())
+    val getAllCollectionsWithMarkerUseCase =
+        GetAllCollectionsWithMarkerUseCase(db.photoCollectionDao())
     val insertMarkerUseCase = InsertMarkerUseCase(db.markerDao(), coroutineScope)
+    val addMarkerOnMapUseCase = AddMarkerOnMapUseCase(db.photoCollectionDao(), coroutineScope)
+    val deleteMarkerUseCase = DeleteMarkerOnMapUseCase(db.photoCollectionDao(), coroutineScope)
 
-    var showMarkerListDialog by remember { mutableStateOf(false) }
-    val models by getAllMarkersUseCase().collectAsStateWithLifecycle(emptyList())
+    var showMarkerListDialogWithAdd by remember { mutableStateOf(false) }
+    val cameraPositionState = rememberCameraPositionState()
+    val markers by getAllMarkersUseCase()
+        .collectAsStateWithLifecycle(emptyList())
+    val collectionMarkers by getAllCollectionsWithMarkerUseCase()
+        .collectAsStateWithLifecycle(emptyList())
 
-    if (showMarkerListDialog) {
-        MarkerListDialog(
-            onDismiss = { showMarkerListDialog = false },
+    if (showMarkerListDialogWithAdd) {
+        MarkerListDialogWithAdd(
+            onDismiss = { showMarkerListDialogWithAdd = false },
             onCompleteMarker = { model -> insertMarkerUseCase(model) },
-            models = models
+            models = markers
         )
     }
 
@@ -78,7 +89,11 @@ fun MainScreen() {
             MapView(
                 modifier = Modifier
                     .padding(innerPadding),
-                getAllMarkersUseCase = getAllMarkersUseCase
+                collectionMarkers = collectionMarkers,
+                cameraPositionState = cameraPositionState,
+                onAddMarker = { addMarkerOnMapUseCase(it) },
+                onRemoveMarker = { deleteMarkerUseCase(it) },
+                markers = markers
             )
         },
         bottomBar = {
@@ -86,7 +101,7 @@ fun MainScreen() {
                 modifier = Modifier
                     .height(100.dp)
                     .fillMaxWidth(),
-                onMarkerListClick = { showMarkerListDialog = true },
+                onMarkerListClick = { showMarkerListDialogWithAdd = true },
                 onCentralClick = {},
                 onTravelClick = {}
             )
